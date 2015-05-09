b6472502ca92835ad939e3fc2bd44642568589c2
rbadetamuc <rbade@leomail.tamuc.edu>
2015-05-09 16:32:31 -0500

    Chiris-Master-Files
    Update as on 08 May

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/* Add here all your JS customizations */
+jQuery(document).ready(function($) {
+	checkSize();
+	$(window).resize(function(){
+   		checkSize();
+	});
+});
+function checkSize(){
+	if($('.check-media').width() != 990){
+	 // Mobile stuff.
+	$('.foot-widget-title').click(function(e) {
+		e.preventDefault();
+		$(this).next().slideToggle();
+		$(this).find('.fa').ToggleClass('');
+	});
+
+	}
+}
